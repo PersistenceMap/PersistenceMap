99ca738b7075a5d08419df6cb5fa54cff0ab99b5
chriswalpen <c.walpen@gmx.net>
2014-07-27 23:34:31 +0200

    Extraced interfaces for mapoptions

---
+++ src/PersistanceMap/QueryProvider/MapOptions/IProcedureMapOption.cs
@@ -0,0 +1,22 @@
+﻿using PersistanceMap.QueryBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistanceMap.QueryProvider
+{
+    public interface IProcedureMapOption
+    {
+        IQueryMap Value<T>(Expression<Func<T>> predicate);
+
+        IQueryMap Value<T>(string name, Expression<Func<T>> predicate);
+    }
+
+    public interface IProcedureMapOption<T>
+    {
+        IQueryMap MapTo<TOut>(string source, Expression<Func<T, TOut>> alias);
+    }
+}
---
+++ src/PersistanceMap/QueryProvider/MapOptions/ISelectMapOption.cs
@@ -0,0 +1,13 @@
+﻿using PersistanceMap.QueryBuilder;
+using System;
+using System.Linq.Expressions;
+
+namespace PersistanceMap.QueryProvider
+{
+    public interface ISelectMapOption<T>
+    {
+        IQueryMap MapTo<TAlias, TOut>(Expression<Func<TAlias, TOut>> source, Expression<Func<T, TOut>> alias);
+
+        //IQueryMap MapTo<TSource, TAlias, TOut>(Expression<Func<TAlias, TOut>> source, Expression<Func<TSource, TOut>> alias);
+    }
+}
