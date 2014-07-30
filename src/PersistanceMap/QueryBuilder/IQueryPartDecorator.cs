6d1559dc53e77ac1960072ddb7fe8c3bf12187b7
chriswalpen <c.walpen@gmx.net>
2014-07-30 10:49:22 +0200

    IQueryPartDecorator

---
+++ src/PersistanceMap/QueryBuilder/IQueryPartDecorator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistanceMap.QueryBuilder
+{
+    public interface IQueryPartDecorator
+    {
+        void Add(IQueryPart part);
+
+        void AddToLast(IQueryPart part, MapOperationType operation);
+
+        void AddToLast(IQueryPart part, Func<IQueryPart, bool> predicate);
+    }
+}
