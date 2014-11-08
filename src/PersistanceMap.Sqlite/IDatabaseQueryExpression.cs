7d6f4116254061ad2d1c63120ac1aeb7bc2fe109
chriswalpen <c.walpen@gmx.net>
2014-11-08 21:51:38 +0100

    Added support for Sqlite

---
+++ src/PersistanceMap.Sqlite/IDatabaseQueryExpression.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Linq.Expressions;
+namespace PersistanceMap.Sqlite
+{
+    public interface IDatabaseQueryExpression : IQueryExpression
+    {
+        void Create();
+
+        //ITableQueryExpression Table(string table);
+        ITableQueryExpression<T> Table<T>();
+    }
+
+    public interface ITableQueryExpression<T> : IQueryExpression
+    {
+        void Create();
+
+        void Update();
+
+        //ITableQueryExpression<T> AddField(string field, string dbType, bool nullable = true);
+
+        //ITableQueryExpression<T> DropField(string field);
+
+        //ITableQueryExpression<T> AlterField(string field);
+
+        ITableQueryExpression<T> Ignore(Expression<Func<T, object>> field);
+
+        ITableQueryExpression<T> Key(params Expression<Func<T, object>>[] keys);
+
+        ITableQueryExpression<T> Key<TRef>(Expression<Func<T, object>> field, Expression<Func<TRef, object>> reference, string name = null);
+    }
+}
---
+++ src/PersistanceMap.Sqlite/QueryBuilder/DatabaseQueryBuilder.cs
@@ -0,0 +1,108 @@
+﻿using PersistanceMap.QueryParts;
+using System;
+
+namespace PersistanceMap.Sqlite.QueryBuilder
+{
+    internal class DatabaseQueryBuilderBase : IQueryExpression
+    {
+        public DatabaseQueryBuilderBase(IDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public DatabaseQueryBuilderBase(IDatabaseContext context, IQueryPartsMap container)
+        {
+            _context = context;
+            _queryPartsMap = container;
+        }
+
+        #region IQueryProvider Implementation
+
+        readonly IDatabaseContext _context;
+        public IDatabaseContext Context
+        {
+            get
+            {
+                return _context;
+            }
+        }
+
+        IQueryPartsMap _queryPartsMap;
+        public IQueryPartsMap QueryPartsMap
+        {
+            get
+            {
+                if (_queryPartsMap == null)
+                    _queryPartsMap = new QueryPartsMap();
+                return _queryPartsMap;
+            }
+        }
+
+        #endregion
+
+    }
+
+    internal class DatabaseQueryBuilder : DatabaseQueryBuilderBase, IDatabaseQueryExpression, IQueryExpression
+    {
+        public DatabaseQueryBuilder(IDatabaseContext context)
+            : base(context)
+        {
+        }
+
+        public DatabaseQueryBuilder(IDatabaseContext context, IQueryPartsMap container)
+            : base(context, container)
+        {
+        }
+        
+        #region IDatabaseQueryExpression Implementation
+
+        public virtual void Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITableQueryExpression<T> Table<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+
+    internal class TableQueryBuilder<T> : DatabaseQueryBuilderBase, ITableQueryExpression<T>
+    {
+        public TableQueryBuilder(IDatabaseContext context, IQueryPartsMap container)
+            : base(context, container)
+        {
+        }
+
+        #region ITableQueryExpression Implementation
+
+        public void Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITableQueryExpression<T> Ignore(System.Linq.Expressions.Expression<Func<T, object>> field)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITableQueryExpression<T> Key(params System.Linq.Expressions.Expression<Func<T, object>>[] keys)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITableQueryExpression<T> Key<TRef>(System.Linq.Expressions.Expression<Func<T, object>> field, System.Linq.Expressions.Expression<Func<TRef, object>> reference, string name = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
