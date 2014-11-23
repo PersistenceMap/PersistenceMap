6f26b25eef0d6675a8b30df1bb2e6a65e55b181d
chriswalpen <c.walpen@gmx.net>
2014-11-23 21:56:49 +0100

    Added value converter

---
+++ src/PersistanceMap/QueryParts/MapValueConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace PersistanceMap.QueryParts
+{
+    public class MapValueConverter
+    {
+        public Expression<Func<object, object>> Converter { get; set; }
+
+        public string ID { get; set; }
+    }
+}
