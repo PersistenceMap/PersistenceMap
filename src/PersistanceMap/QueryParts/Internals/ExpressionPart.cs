c274d64c1e7a52f00a9363114b95cfc6113a53df
chriswalpen <c.walpen@gmx.net>
2014-11-27 23:04:14 +0100

    Refactored to Delegatepart

---
+++ src/PersistanceMap/QueryParts/Internals/ExpressionPart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace PersistanceMap.QueryParts
+{
+    class ExpressionPart : IExpressionPart
+    {
+        public ExpressionPart(LambdaExpression expression)
+        {
+            Expression = expression;
+            AliasMap = new Dictionary<Type,string>();
+        }
+
+        public LambdaExpression Expression { get; private set; }
+
+        public Dictionary<Type, string> AliasMap { get; private set; }
+    }
+}
