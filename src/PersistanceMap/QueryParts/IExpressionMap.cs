a91371ee4d7b4b66e25a8113f448679cc4820bcc
chriswalpen <c.walpen@gmx.net>
2014-11-28 08:00:22 +0100

    Removed ExpressionQueryPart

---
+++ src/PersistanceMap/QueryParts/IExpressionMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace PersistanceMap.QueryParts
+{
+    /// <summary>
+    /// Defines a Interface that is used to add alias mappings to a expression. This gets used when compiling the expression to a sql string
+    /// </summary>
+    public interface IExpressionMap
+    {
+        /// <summary>
+        /// The expression that contains alias mappings
+        /// </summary>
+        LambdaExpression Expression { get; }
+
+        /// <summary>
+        /// Defines a mapping for types and the alias that the entity has
+        /// </summary>
+        Dictionary<Type, string> AliasMap { get; }
+    }
+}
