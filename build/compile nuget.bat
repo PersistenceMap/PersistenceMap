b06b8a37765afd2969ff1923285038c4c1d67d64
chriswalpen <c.walpen@gmx.net>
2014-12-10 22:03:30 +0100

    Added output to compile bat

--- build/compile nuget.bat
+++ build/compile nuget.bat
@@ -1,7 +1,10 @@
 @echo off
 
+echo Begin create nuget for PersistanceMap
 nuget.exe pack ..\src\PersistanceMap.nuspec
 
+echo Begin create nuget for PersistanceMap.Sqlite
 nuget.exe pack ..\src\PersistanceMap.Sqlite.nuspec
 
+echo End create nuget for PersistanceMap
 pause
