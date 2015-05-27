b31c3f33d80c05e02a4b08de45d63aba1204b03a
chriswalpen <c.walpen@gmx.net>
2015-05-27 22:57:22 +0200

    Release 0.4.0

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -23,4 +23,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.1")]
+[assembly: AssemblyFileVersion("0.4.0.4")]
