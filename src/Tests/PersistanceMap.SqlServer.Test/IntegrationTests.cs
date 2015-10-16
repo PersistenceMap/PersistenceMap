5d7727de4b57d2c08fe7dece044f64449c2305d2
chriswalpen <c.walpen@gmx.net>
2015-10-16 08:17:07 +0200

    Added LocalDb POC for unittests

--- src/Tests/PersistanceMap.SqlServer.Test/IntegrationTests.cs
+++ src/Tests/PersistanceMap.SqlServer.Test/IntegrationTests.cs
@@ -1,20 +1,53 @@
-﻿using NUnit.Framework;
-using PersistanceMap.Test.TableTypes;
-using System;
+﻿using System.IO;
 using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PersistanceMap.Test;
+using PersistanceMap.Test.LocalDb;
+using PersistanceMap.Test.TableTypes;
 
-namespace PersistanceMap.Test.Expression
+namespace PersistanceMap.SqlServer.Test
 {
-    [TestFixture]
-    public class DeleteExpressionTests : TestBase
+    [TestClass]
+    public class IntegrationTests : TestBase
     {
-        [Test]
-        [Description("A failing delete satement that defines the deletestatement according to the values from a distinct Keyproperty of a given entity")]
-        public void DeleteEntityWithExpressionKeyThatFails()
+        private static LocalDbManager _localDbManager;
+
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
         {
-            Assert.Throws<ArgumentException>(() => new SqlContextProvider(null));
+            _localDbManager = new LocalDbManager("Northwind");
+
+            var file = new FileInfo(@"AppData\Nothwind.SqlServer.sql");
+            string script = file.OpenText().ReadToEnd();
+            _localDbManager.ExecuteString(script);
         }
 
-        
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            _localDbManager.Dispose();
+        }
+
+        [TestMethod]
+        public void LocalDbTest()
+        {
+            var provider = new SqlContextProvider(_localDbManager.ConnectionString);
+            using (var context = provider.Open())
+            {
+                var query = context.From<Orders>().Map(o => o.OrdersID).Join<OrderDetails>((d, o) => d.OrdersID == o.OrdersID);
+
+                var sql = "SELECT Orders.OrdersID, ProductID, UnitPrice, Quantity, Discount FROM Orders JOIN OrderDetails ON (OrderDetails.OrdersID = Orders.OrdersID)";
+                var expected = query.CompileQuery();
+
+                // check the compiled sql
+                Assert.AreEqual(expected.Flatten(), sql);
+
+                // execute the query
+                var orders = query.Select();
+
+                Assert.IsNotNull(orders);
+                Assert.IsTrue(orders.Any());
+            }
+        }
     }
 }
